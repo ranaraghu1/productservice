ac5d23d035276a0fd1a2db9fe6086d4e3a703c4b
raghu <rn@gmail.com>
2021-12-28 23:26:21 +0530

    Health check and metrics

---
+++ src/main/java/com/rana/springboot/healthcheck/CustomHealthIndicator.java
@@ -0,0 +1,20 @@
+package com.rana.springboot.healthcheck;
+
+//localhost:8080/productapi/actuator/health
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		boolean error= true;
+		if(error) {
+			return Health.down().withDetail("Error Key", 123).build();
+		}
+		return Health.up().build();
+	}
+
+}
