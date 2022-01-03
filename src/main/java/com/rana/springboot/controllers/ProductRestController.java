7662f102c3a104e6cdd12c2e9c2d84e8d264a648
raghu <rn@gmail.com>
2022-01-03 21:29:09 +0530

    Exception Handling-ResponseEntityExceptionHandler

--- src/main/java/com/rana/springboot/controllers/ProductRestController.java
+++ src/main/java/com/rana/springboot/controllers/ProductRestController.java
@@ -1,5 +1,7 @@
 package com.rana.springboot.controllers;
 
+
+import java.net.URI;
 import java.util.List;
 
 
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +22,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rana.springboot.entites.Product;
+import com.rana.springboot.exception.UserNotFoundException;
 import com.rana.springboot.repos.ProductRepository;
 
 @RestController
@@ -43,10 +48,31 @@ public class ProductRestController {
 	@Cacheable("product-cache")
 	public Product getProductById(@PathVariable("id") int id) {
 		 LOGGER.info("Finding product by ID:"+id);
-		return repository.findById(id).get();
-
+		 
+		Product product= repository.findById(id).get();
+		if(product==null) {
+			throw new UserNotFoundException("Id-"+id);
+		}
+		else {
+		   return product;
+		}
+        
 	}
+	
+   // Return 201 status code as response for created user
+	
+	@PostMapping(value = "/products-status/")
+	public ResponseEntity<Object> createProductStatusResponse(@Valid @RequestBody Product product) {
 
+		Product products= repository.save(product);
+		
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(products.getId()).toUri();
+		
+	     return ResponseEntity.created(location).build();
+	}
+	
 	@PostMapping(value = "/products/")
 	public Product createProduct(@Valid @RequestBody Product product) {
 
