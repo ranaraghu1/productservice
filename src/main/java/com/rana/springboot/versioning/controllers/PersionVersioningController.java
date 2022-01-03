1bdbf064ef687e44c6e3b0e2cf7f1922045c717c
raghu <rn@gmail.com>
2022-01-03 13:05:32 +0530

    versioning

--- src/main/java/com/rana/springboot/versioning/controllers/PersionVersioningController.java
+++ src/main/java/com/rana/springboot/versioning/controllers/PersionVersioningController.java
@@ -16,5 +16,35 @@ public class PersionVersioningController {
 	public PersonV2 personV2() {
 		return new  PersonV2("Raghu","Rana");
 	}
+	
+	
+	@GetMapping(value="/person/param",params="version=1")
+	public PersonV1 param1() {
+		return new  PersonV1("Raghu Rana");
+	}
+	@GetMapping(value="/person/param",params="version=2")
+	public PersonV2 param2() {
+		return new  PersonV2("Raghu","Rana");
+	}
 
+	
+
+	@GetMapping(value="/person/header",headers="X-API-VERSION=1")
+	public PersonV1 header1() {
+		return new  PersonV1("Raghu Rana");
+	}
+	@GetMapping(value="/person/header",headers="X-API-VERSION=2")
+	public PersonV2 header2() {
+		return new  PersonV2("Raghu","Rana");
+	}
+	
+	
+	@GetMapping(value="/person/produces",produces="application/vnd.company.add-v1+json")
+	public PersonV1 produces1() {
+		return new  PersonV1("Raghu Rana");
+	}
+	@GetMapping(value="/person/produces",produces="application/vnd.company.add-v2+json")
+	public PersonV2 produces2() {
+		return new  PersonV2("Raghu","Rana");
+	}
 }
